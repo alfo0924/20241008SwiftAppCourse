3173a7d912f122dc2cc1cbaf8e81fa334fa98084
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-10-08 10:01:34 +0800

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUiText20241008
+//
+//  Created by 訪客使用者 on 2024/10/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUiText20241008App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiText20241008App.swift
+//  SwiftUiText20241008
+//
+//  Created by 訪客使用者 on 2024/10/8.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiText20241008App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
